8d1ab01d4534385dc36b2eb1e3729dc0b9695dcd
wonsunahn <wahn@pitt.edu>
2021-02-06 16:06:10 -0500

    initial commit

---
+++ sample_code/junit_cat_scale/CatScaleTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+
+import org.mockito.*;
+
+public class CatScaleTest {
+	/**
+	 * Test case for String report(Cat cat).
+	 * Preconditions: A new CatScale is created and assigned to "scale".
+	 *                A new Cat weighing 80 pounds is created and assigned to "cat".
+	 * Execution steps: Call scale.report(cat).
+	 * Postconditions: Return value is "Overweight".
+	 *                 cat.getWeight() has been called.
+	 */
+	@Test
+	public void testReport() {
+		CatScale scale = new CatScale();
+		Cat cat = Mockito.mock(Cat.class);
+		Mockito.when(cat.getWeight()).thenReturn(80);
+		assertEquals("Overweight", scale.report(cat));
+		Mockito.verify(cat).getWeight();
+	}
+}
