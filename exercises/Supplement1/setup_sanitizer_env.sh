954e43e52416b850fa04db1438032c8eedf4a5e0
wonsunahn <wahn@pitt.edu>
2021-02-10 10:17:26 -0500

    Updates

--- exercises/Supplement1/setup_sanitizer_env.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$PATH:/afs/cs.pitt.edu/courses/1632/clang-9.0.1/bin/
-export ASAN_OPTIONS=detect_stack_use_after_return=1
--- exercises/Supplement1/stack_overflow.c
+++ exercises/Supplement1/stack_overflow.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct _Node {
+  struct _node *next;
+  unsigned char data[8];
+} Node;
+
 void send_data(unsigned char *data, int len) {
   for (int i=0; i < len; i++) {
     printf("%2x ", data[i]);
@@ -10,10 +15,15 @@ void send_data(unsigned char *data, int len) {
 }
 
 int main() {
-  unsigned char *p;
-  unsigned char data[8] = {0};
-  p = data;
-  printf("p = %p\n", p);
-  send_data(data, 16);
+  Node first, second, third;
+  first.next = &second;
+  strcpy(first.data, "Hello");
+  second.next = &third;
+  strcpy(second.data, "World");
+  third.next = NULL;
+  strcpy(second.data, "!");
+
+  printf("second.next = %p\n", second.next);
+  send_data(first.data, 16);
   return 0;
 }
