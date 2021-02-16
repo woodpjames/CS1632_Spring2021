260594d3fa38d14886f3c9368c83e1095be02285
wonsunahn <wahn@pitt.edu>
2021-02-16 16:40:47 -0500

    Update to force create the bin directory

--- exercises/2/run.sh
+++ exercises/2/run.sh
@@ -1,3 +1,5 @@
+mkdir -f bin
+
 javac -cp CommandLineJunit/junit-4.12.jar:CommandLineJunit/hamcrest-core-1.3.jar:CommandLineJunit/mockito-core-1.10.19.jar:CommandLineJunit/objenesis-2.4.jar -d bin src/*.java
 
 java -cp bin:CommandLineJunit/junit-4.12.jar:CommandLineJunit/hamcrest-core-1.3.jar:CommandLineJunit/mockito-core-1.10.19.jar:CommandLineJunit/objenesis-2.4.jar TestRunner
