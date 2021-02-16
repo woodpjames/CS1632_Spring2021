c1afd1469e881f5ce2967a4be8d724eb8746eaf6
wonsunahn <wahn@pitt.edu>
2021-02-16 16:47:20 -0500

    Update run.sh

--- exercises/2/run.sh
+++ exercises/2/run.sh
@@ -1,4 +1,4 @@
-mkdir -f bin
+mkdir bin
 
 javac -cp CommandLineJunit/junit-4.12.jar:CommandLineJunit/hamcrest-core-1.3.jar:CommandLineJunit/mockito-core-1.10.19.jar:CommandLineJunit/objenesis-2.4.jar -d bin src/*.java
 
